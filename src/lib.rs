da7bdb7f2bc0ae0405be92d85d5bfc7cf2d895b9
Stephan Hügel <urschrei@gmail.com>
2018-04-10 00:39:47 +0100

    Typo fix

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 //! ### A note on Coordinate Order
 //! While individual providers may specify coordinates in either `[Longitude, Latitude]` **or**
 //! `[Latitude, Longitude`] order,
-//! `Geocoding` **always** requires `Point` data in `[Longitude, Latitude] (`x, y`) order,
+//! `Geocoding` **always** requires `Point` data in `[Longitude, Latitude]` (`x, y`) order,
 //! and returns data in that order.
 
 extern crate geo;
